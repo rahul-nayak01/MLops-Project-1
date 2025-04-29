70aacf5f2636523c521bc6074544cd25f93cd195
Rahul <nayakrahul085@gmail.com>
2025-04-29 17:47:20 +0530

    project structure ready , venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rahul R nayak",
+    author_email="nayakrahul0852@gmail.com",
+    packages=find_packages()
+)
